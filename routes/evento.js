bdddd79ee6c57d1c00aca8c1d254ac6e200785dc
Pepe Valdivia <jvaldivia@softweb.pe>
2018-03-17 07:16:07 -0500

    Editar evento

--- routes/evento.js
+++ routes/evento.js
@@ -10,23 +10,13 @@ router.get('/listar', function(req, res, next) {
       res.send(err);
     }
     var rs = [];
-    /*
-    :nombre => 'Biopolímero a partir del almidón de papa',
-    :nombre_url => 'biopolimero-a-partir-del-almidón-de-papa',
-    :dia_inicio => '22/07/2015',
-    :dia_fin => '22/07/2015',
-    :hora_inicio => '14.00',
-    :hora_fin => '16:00',
-    :lugar => 'Auditorio Edificio S',
-    :direccion => 'Avenida Javier Prado Este N.° 4600, Santiago de Surco, Lima, Perú '
-    */
     while (cursor.next()) {
       var date_temp1 = new Date(cursor.field('dia_inicio'));
       var inicio = dateFormat(date_temp1, 'dd/mm/yyyy');
       var date_temp2 = new Date(cursor.field('dia_fin'));
       var fin = dateFormat(date_temp2, 'dd/mm/yyyy');
       var data = {
-        'id' : cursor.field('_id'),
+        '_id' : cursor.field('_id'),
         'nombre' : cursor.field('nombre'),
         'nombre_url' : cursor.field('nombre_url'),
         'dia_inicio' : inicio,
@@ -56,5 +46,18 @@ router.post('/crear', function(req, res, next) {
   });
 });
 
+router.post('/editar', function(req, res, next) {
+  var document = JSON.parse(req.query.evento);
+  document['dia_inicio'] = helpers.fechaViewToJS(document['dia_inicio']);
+  document['dia_fin'] = helpers.fechaViewToJS(document['dia_fin']);
+  db.conn.update('eventos', {'_id': document['_id'], $set : document}, function(err, oids) {
+    if (err) {
+      var rpta = {'tipo_mensaje' :  'error', 'mensaje' : ['Se ha producido un error en editar el evento', err]};
+      res.send(JSON.stringify(rpta));
+    }
+    var rpta = {'tipo_mensaje' :  'success', 'mensaje' : ['Se ha editado el evento']};
+    res.send(JSON.stringify(rpta));
+  });
+});
 
 module.exports = router;
--- tests/rspec/evento.rb
+++ tests/rspec/evento.rb
@@ -37,4 +37,41 @@ def crear
   end
 end
 
-crear
+def editar
+  RSpec.describe App do
+    describe '2. Editar evento: ' do
+      it '2.1 Conexión con backend' do
+        url = 'test/conexion'
+        test = App.new(url)
+        test.get()
+        expect(test.response.code).to eq(200)
+      end
+      it '2.2 Editar evento' do
+        data = {
+          :_id => '5aad032db409546800000000',
+          :nombre => 'Biopolímero a partir del almidón de papa XD',
+          :nombre_url => 'biopolimero-a-partir-del-almidón-de-papa-xd',
+          :dia_inicio => '22/07/2013',
+          :dia_fin => '22/07/2013',
+          :hora_inicio => '14.02',
+          :hora_fin => '16:03',
+          :lugar => 'Auditorio Edificio W',
+          :direccion => 'Avenida Javier Prado Este N.° 4600, Santiago de Surco, Lima, Perú '
+        }.to_json
+        url = 'evento/editar?evento=' + data
+        test = App.new(url)
+        test.post()
+        if test.response.code != 200 then
+          puts test.response.body
+        end
+        expect(test.response.code).to eq(200)
+        expect(test.response.body).not_to include('error')
+        expect(test.response.body).to include('Se ha editado el evento')
+        expect(test.response.body).to include('success')
+      end
+    end
+  end
+end
+
+#crear
+editar
