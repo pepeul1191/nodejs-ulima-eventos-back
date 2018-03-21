39239f70c45e7c0048c7dc1d1b792c79ab7edf85
Pepe Valdivia <jvaldivia@softweb.pe>
2018-03-21 10:43:24 -0500

    Obtener evento por ID

--- routes/evento.js
+++ routes/evento.js
@@ -103,6 +103,33 @@ router.get('/nombre_url', function(req, res, next) {
         'hora_fin' : cursor.field('hora_fin'),
         'lugar' : cursor.field('lugar'),
         'direccion' : cursor.field('direccion'),
+        'descripcion' : cursor.field('descripcion'),
+      };
+    }
+    res.send(JSON.stringify(rpta));
+  });
+});
+
+router.get('/_id', function(req, res, next) {
+  var _id = req.query._id;
+  db.conn.find('eventos', {'_id': _id}, function(err, cursor) {
+    var rpta = null;
+    while (cursor.next()) {
+      var date_temp1 = new Date(cursor.field('dia_inicio'));
+      var inicio = dateFormat(date_temp1, 'dd/mm/yyyy');
+      var date_temp2 = new Date(cursor.field('dia_fin'));
+      var fin = dateFormat(date_temp2, 'dd/mm/yyyy');
+      rpta = {
+        '_id' : cursor.field('_id'),
+        'nombre' : cursor.field('nombre'),
+        'nombre_url' : cursor.field('nombre_url'),
+        'dia_inicio' : inicio,
+        'dia_fin' : fin,
+        'hora_inicio' : cursor.field('hora_inicio'),
+        'hora_fin' : cursor.field('hora_fin'),
+        'lugar' : cursor.field('lugar'),
+        'direccion' : cursor.field('direccion'),
+        'descripcion' : cursor.field('descripcion'),
       };
     }
     res.send(JSON.stringify(rpta));
